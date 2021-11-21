fa1e7080128fb0694cffffd133e01c79d656b0b6
Sarah German <sarah@clockwork.com>
2021-11-21 16:12:42 -0600

    Add content and styling from old site.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
+const faviconPlugin = require("eleventy-favicon");
+const eleventyGoogleFonts = require("eleventy-google-fonts");
+
 module.exports = function (eleventyConfig) {
+  // Pass static assets to /public.
+  eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/css");
+
+  // Rebuild on file change.
   eleventyConfig.addWatchTarget("./src/css/");
+
+  // Plugins.
+  eleventyConfig.addPlugin(faviconPlugin, {destination: "./public"});
+  eleventyConfig.addPlugin(eleventyGoogleFonts);
 
   return {
     dir: {
